e2474246b025b0a1a758beccd4b191e1b7ff20d0
Your Name <you@example.com>
2024-06-05 21:06:24 +0300

    added

--- lesson3 - MySQL/queries.sql
+++ lesson3 - MySQL/queries.sql
@@ -5,11 +5,32 @@ SELECT
     COUNT(test_grades.id) 'gradeAmount'
 FROM
     students
-LEFT JOIN test_grades ON students.id = test_grades.studentId
+LEFT JOIN
+    test_grades ON students.id = test_grades.studentId
 GROUP BY
     students.id;
 
 -- רשימה של סטודנטים עם ממוצע מבחנים
-
+SELECT
+    students.firstName,
+    students.lastName,
+    AVG(test_grades.grade) 'average'
+FROM
+    students
+LEFT JOIN
+    test_grades ON students.id = test_grades.studentId
+GROUP BY
+    students.id;
 
 -- רשימה של מבחנים עם ממוצע ציונים וכמות נבחנים
+SELECT
+    tests.name,
+    tests.date,
+    AVG(test_grades.grade) 'average',
+    COUNT(test_grades.id) 'studentAmount'
+FROM
+    tests
+LEFT JOIN
+    test_grades ON tests.id = test_grades.testId
+GROUP BY
+    tests.id;
