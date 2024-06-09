7dd01a564f7c0c6e5b4868eeb378bb0f6535482f
Your Name <you@example.com>
2024-06-09 20:13:43 +0300

    added

--- lesson4 - MySQL/queries.sql
+++ lesson4 - MySQL/queries.sql
@@ -4,12 +4,22 @@ LEFT JOIN test_grades AS g ON s.id = g.studentId
 GROUP BY city;
 
 -- כמה סטודנטים קיבלו מכל ציון
+SELECT grade, COUNT(*) FROM test_grades GROUP BY grade;
 
+-- כמה סטודנטים קיבלו מכל ציון + פירוט של השמות
+SELECT g.grade, COUNT(*) AS amount, GROUP_CONCAT(s.firstName) AS names
+FROM test_grades AS g
+LEFT JOIN students AS s ON g.studentId = s.id
+GROUP BY g.grade;
 
 -- ממוצע ציונים לפי מבחן
-
+SELECT t.name, AVG(g.grade) AS grade FROM tests AS t
+LEFT JOIN test_grades AS g ON t.id = g.testId
+GROUP BY t.id;
 
 -- שלושת הסטודנטים שקיבלו את הציון הנמוך
-
+SELECT s.firstName, s.lastName, g.grade FROM test_grades AS g
+LEFT JOIN students AS s ON g.studentId = s.id
+ORDER BY g.grade ASC LIMIT 3;
 
 -- חמשת הסטודנטים שקיבלו את הציון הגבוה
