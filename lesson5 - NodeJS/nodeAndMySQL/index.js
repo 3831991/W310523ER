904255248186f288153b3f04557c1205a5946db5
Your Name <you@example.com>
2024-06-16 21:02:26 +0300

    added

--- lesson5 - NodeJS/nodeAndMySQL/index.js
+++ lesson5 - NodeJS/nodeAndMySQL/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const app = express();
 
+// יצירת הגדרות החיבור למסד הנתונים
 const con = mysql.createConnection({
     host: 'localhost',
     // port: 8889, // למחשבי Mac
@@ -10,6 +11,7 @@ const con = mysql.createConnection({
     database: 'full-stack-w310523er',
 });
 
+// התחברות למסד הנתונים
 con.connect(err => {
     if (err) {
         throw err;
@@ -18,6 +20,7 @@ con.connect(err => {
     console.log('Connecting to mysql');
 });
 
+// הפעלת השרת והאזנה לפורט הנבחר
 app.listen(5000, () => {
     console.log('listening on port 5000');
 });
@@ -25,5 +28,15 @@ app.listen(5000, () => {
 app.get('/', (req, res) => {
     res.send({
         hello: 'Hello world!',
+    });
+});
+
+app.get("/students", (req, res) => {
+    con.query("SELECT * FROM students", (err, result) => {
+        if (err) {
+            throw err;
+        }
+
+        res.send(result);
     });
 });
