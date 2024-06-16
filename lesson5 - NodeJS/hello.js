69f42aa290ecc4f56a790755fa0060e5e12deb68
Your Name <you@example.com>
2024-06-16 20:03:50 +0300

    added

--- lesson5 - NodeJS/hello.js
+++ lesson5 - NodeJS/hello.js
@@ -1,3 +1,38 @@
-const x = "Hello world!";
+// ייבוא האקספרס
+const express = require('express');
 
-console.log(x);
+// שימוש באקספרס
+const app = express();
+
+// הפעלת השרת (והגדרת הפורט)
+app.listen(3875, () => {
+    console.log('listening on port 3875');
+});
+
+// יירוט הבקשה
+// כשפונים לשרת ב-GET
+// ולא מוסיחפים ניתוב מיוחד
+app.get('/', (req, res) => {
+    res.send({
+        message: "Hello world!",
+    });
+});
+
+// יירוט הבקשה
+// כשפונים לשרת ב-GET
+// ויש ניתוב ל-students
+app.get('/students', (req, res) => {
+    res.send([
+        { firstName: 'John', lastName: 'John' },
+        { firstName: 'John', lastName: 'John' },
+        { firstName: 'John', lastName: 'John' },
+        { firstName: 'John', lastName: 'John' },
+    ]);
+});
+
+// כנ"ל
+app.get('/test', (req, res) => {
+    res.send({
+        message: "Hello world 2",
+    });
+});
