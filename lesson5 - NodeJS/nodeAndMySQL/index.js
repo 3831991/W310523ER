76ed26585b387e939e95b094920338bc8437d95d
Your Name <you@example.com>
2024-06-16 20:44:06 +0300

    added

--- lesson5 - NodeJS/nodeAndMySQL/index.js
+++ lesson5 - NodeJS/nodeAndMySQL/index.js
@@ -1,5 +1,22 @@
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    // port: 8889, // למחשבי Mac
+    user: 'root',
+    password: '',
+    database: 'full-stack-w310523er',
+});
+
+con.connect(err => {
+    if (err) {
+        throw err;
+    }
+
+    console.log('Connecting to mysql');
+});
 
 app.listen(5000, () => {
     console.log('listening on port 5000');
