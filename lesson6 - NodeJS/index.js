f578378cef44139e95f902b47a195ad77df01624
Your Name <you@example.com>
2024-06-19 18:19:45 +0300

    added

--- lesson6 - NodeJS/index.js
+++ lesson6 - NodeJS/index.js
@@ -40,3 +40,25 @@ app.get("/students", (req, res) => {
         res.send(result);
     });
 });
+
+app.get("/students/average", (req, res) => {
+    con.query(`
+        SELECT
+            s.firstName,
+            s.lastName,
+            AVG(tg.grade) AS average
+        FROM
+            students AS s
+        LEFT JOIN test_grades AS tg
+        ON
+            tg.studentId = s.id
+        GROUP BY
+            s.id
+    `, (err, result) => {
+        if (err) {
+            throw err;
+        }
+
+        res.send(result);
+    });
+});
