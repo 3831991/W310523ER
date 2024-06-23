0eaf9494b7c039ddd833011e9f72550af5b4588e
Your Name <you@example.com>
2024-06-23 19:01:20 +0300

    added

--- lesson6 - NodeJS/frontend/src/Cities.js
+++ lesson6 - NodeJS/frontend/src/Cities.js
@@ -1,12 +1,20 @@
 import { useEffect, useState } from 'react';
 
 export default function Cites() {
-    const [students, setStudents] = useState([]);
+    const [cities, setCities] = useState([]);
+    const [min, setMin] = useState([]);
+    const [max, setMax] = useState([]);
     
     useEffect(() => {
         fetch("http://localhost:5000/students/average-by-cites")
         .then(res => res.json())
-        .then(data => setStudents(data));
+        .then(data => {
+            setCities(data);
+
+            const numbers = data.map(x => +x.AVG);
+            setMin(Math.min(...numbers));
+            setMax(Math.max(...numbers));
+        });
     }, []);
 
     return (
@@ -20,11 +28,11 @@ export default function Cites() {
             </thead>
             <tbody>
                 {
-                    students.map((s, i) => 
-                        <tr key={s.id}>
+                    cities.map((s, i) => 
+                        <tr key={s.city}>
                             <td>{i + 1}</td>
                             <td>{s.city}</td>
-                            <td>{s.AVG}</td>
+                            <td className={s.AVG == min ? 'min' : s.AVG == max ? 'max' : ''}>{s.AVG}</td>
                         </tr>
                     )
                 }
