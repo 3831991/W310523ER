5043a6fd43e361978c41920bec91dfc6194a0e07
Your Name <you@example.com>
2024-07-07 21:19:08 +0300

    added

--- lesson10 - NodeJS/app.mjs
+++ lesson10 - NodeJS/app.mjs
@@ -9,6 +9,12 @@ app.use(cors({
     allowedHeaders: 'Content-Type, Accept',
 }));
 
+app.use((req, res, next) => {
+    console.log(req.method, req.url);
+
+    next();
+});
+
 app.listen(4444, () => {
     console.log("Server is running on port 4444");
 });
