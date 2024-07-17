9515a5ce8745c0fa9687ba91e3c84ca67d392649
Your Name <you@example.com>
2024-07-17 20:45:32 +0300

    added

--- lesson12 - MongoDB/handlers/users/users.mjs
+++ lesson12 - MongoDB/handlers/users/users.mjs
@@ -7,3 +7,16 @@ const User = mongoose.model("users", userSchema);
 app.get("/users", async (req, res) => {
     res.send(await User.find());
 });
+
+app.post("/users", async (req, res) => {
+    const user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        phone: req.body.phone,
+    });
+
+    const newUser = await user.save();
+
+    res.send(newUser);
+});
