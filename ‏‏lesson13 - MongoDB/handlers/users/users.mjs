8708495963a58694ff22c189c8850b465aa0f4a8
Your Name <you@example.com>
2024-07-21 18:59:30 +0300

    added

--- ‏‏lesson13 - MongoDB/handlers/users/users.mjs
+++ ‏‏lesson13 - MongoDB/handlers/users/users.mjs
@@ -5,6 +5,16 @@ app.get("/users", async (req, res) => {
     res.send(await User.find());
 });
 
+app.get("/users/:id", async (req, res) => {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+        return res.status(403).send({ message: "User not found" });
+    }
+
+    res.send(user);
+});
+
 app.post("/users", async (req, res) => {
     const user = new User({
         firstName: req.body.firstName,
@@ -17,3 +27,33 @@ app.post("/users", async (req, res) => {
 
     res.send(newUser);
 });
+
+app.put("/users/:id", async (req, res) => {
+    const { firstName, lastName, email, phone } = req.body;
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+        return res.status(403).send({ message: "User not found" });
+    }
+
+    user.firstName  = firstName;
+    user.lastName   = lastName;
+    user.email      = email;
+    user.phone      = phone;
+
+    await user.save();
+
+    res.send(user);
+});
+
+// שיטה נוספת ויותר נוחה (בהמלצת מיכאל וריאן)
+// app.put("/users/:id", async (req, res) => {
+//     const { firstName, lastName, email, phone } = req.body;
+//     const user = await User.findByIdAndUpdate(req.params.id, { firstName, lastName, email, phone });
+
+//     if (!user) {
+//         return res.status(403).send({ message: "User not found" });
+//     }
+
+//     res.send(user);
+// });
