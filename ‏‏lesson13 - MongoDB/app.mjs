b32356f0b85d4485749fc0942a77fa8e8ddff205
Your Name <you@example.com>
2024-07-21 18:31:58 +0300

    added

---
+++ ‏‏lesson13 - MongoDB/app.mjs
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/full-stack-W310523ER');
+    console.log('mongodb connection established on port 27017');
+}
+
+main().catch(err => console.log(err));
+
+export const app = express();
+
+app.use(express.json());
+
+app.use(cors({
+    origin: true,
+    credentials: true,
+    methods: 'GET,PUT,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+}));
+
+app.listen(8989, () => {
+    console.log('listening on port 8989');
+});
+
+app.get('/', (req, res) => {
+    res.send("Welcome to MongoDB!");
+});
+
+import("./handlers/users/users.mjs");
