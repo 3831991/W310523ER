6ea50af4e5ecb1e44a175eaa19ee8979c7d9682b
Your Name <you@example.com>
2024-07-31 20:02:03 +0300

    added

--- ‏‏‏‏‏‏lesson15 - MongoDB - JWT+Joi/handlers/users/users.joi.mjs
+++ ‏‏‏‏‏‏lesson15 - MongoDB - JWT+Joi/handlers/users/users.joi.mjs
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
-const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d{4})(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{9,30}$/;
+const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{9,30}$/;
 
 export const UserLogin = Joi.object({
     email: Joi.string().required(),
@@ -10,7 +10,7 @@ export const UserLogin = Joi.object({
 export const UserSignup = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().required().regex(passwordRegex),
-    firstName: Joi.string().required().alphanum().min(2).max(20),
-    lastName: Joi.string().required().alphanum().min(2).max(20),
+    firstName: Joi.string().required().min(2).max(20),
+    lastName: Joi.string().required().min(2).max(20),
     phone: Joi.string().required().min(9).max(15),
 });
--- ‏‏‏‏‏‏lesson15 - frontend/src/auth/Signup.js
+++ ‏‏‏‏‏‏lesson15 - frontend/src/auth/Signup.js
@@ -13,38 +13,26 @@ export default function Signup() {
     const [loginError, setLoginError] = useState('');
     const navigate = useNavigate();
 
-    const signup = ev => {
+    const signup = async ev => {
         ev.preventDefault();
         
-        fetch("http://localhost:4444/signup", {
+        const res = await fetch("http://localhost:8989/signup", {
             credentials: 'include',
             method: "POST",
             headers: {
                 'Content-type': 'application/json'
             },
             body: JSON.stringify(formData),
-        })
-        .then(res => {
-            if (res.ok) {
-                return res.json();
-            } else {
-                return res.text().then(x => {
-                    throw new Error(x);
-                });
-            }
-        })
-        .then(data => {
-            navigate('/');
-        })
-        .catch(err => {
-            setLoginError(err.message);
-        })
-        .finally(() => {
-
         });
+
+        if (res.ok) {
+            navigate('/');
+        } else {
+            setLoginError(await res.text());
+        }
     }
 
-    const handleError = ev => {
+    const dataChange = ev => {
         const { id, value } = ev.target;
 
         const obj = {
@@ -63,27 +51,27 @@ export default function Signup() {
                 <form onSubmit={signup}>
                     <label>
                         שם פרטי:
-                        <input type="text" id='firstName' value={formData.firstName} onChange={handleError} />
+                        <input type="text" id='firstName' value={formData.firstName} onChange={dataChange} />
                     </label>
 
                     <label>
                         שם משפחה:
-                        <input type="text" id='lastName' value={formData.lastName} onChange={handleError} />
+                        <input type="text" id='lastName' value={formData.lastName} onChange={dataChange} />
                     </label>
 
                     <label>
                         טלפון:
-                        <input type="tel" id='phone' value={formData.phone} onChange={handleError} />
+                        <input type="tel" id='phone' value={formData.phone} onChange={dataChange} />
                     </label>
 
                     <label>
                         אימייל:
-                        <input type="email" id='email' value={formData.email} onChange={handleError} />
+                        <input type="email" id='email' value={formData.email} onChange={dataChange} />
                     </label>
 
                     <label>
                         סיסמה:
-                        <input type="password" id='password' value={formData.password} onChange={handleError} />
+                        <input type="password" id='password' value={formData.password} onChange={dataChange} />
                     </label>
 
                     <button>הרשם</button>
