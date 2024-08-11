51eabdaf16b61c6868472e899f919f3f3c8a2c73
Your Name <you@example.com>
2024-08-11 20:21:32 +0300

    added

--- memory-game/frontend/script.js
+++ memory-game/frontend/script.js
@@ -1,15 +1,21 @@
 const board = document.getElementById('board');
+const cards = [];
 
 (async () => {
     const res = await fetch("/images");
     const images = await res.json();
 
     images.forEach((image, i) => {
-        const div = document.createElement("div");
-        div.style.backgroundImage = `url("/images/${image}")`;
-        board.appendChild(div);
+        // הוספנו פעמיים
+        cards.push({ i, image }, { i, image });
+    });
 
-        const div2 = div.cloneNode();
-        board.appendChild(div2);
+    cards.sort((a, b) => Math.random() - 0.5);
+
+    cards.forEach(c => {
+        const div = document.createElement("div");
+        div.style.backgroundImage = `url("/images/${c.image}")`;
+        board.appendChild(div);
     });
 })();
+
