84af3f78cf6661b0330eafa43f96c7adb0ff8361
Your Name <you@example.com>
2024-08-11 21:05:35 +0300

    added

--- memory-game/frontend/script.js
+++ memory-game/frontend/script.js
@@ -1,7 +1,11 @@
-const board = document.getElementById('board');
 const cards = [];
+let player1 = 0;
+let player2 = 0;
+let isFirstPlayer = true;
 
 (async () => {
+    const board = document.getElementById('board');
+
     const res = await fetch("/images");
     const images = await res.json();
 
@@ -16,10 +20,57 @@ const cards = [];
         const div = document.createElement("div");
         div.style.backgroundImage = `url("/images/${c.image}")`;
         board.appendChild(div);
+        c.div = div;
 
         div.addEventListener("click", () => {
-            div.classList.add("showed");
+            if (c.showed) {
+                return;
+            }
+
+            const showed = cards.filter(c => c.showed);
+
+            if (showed.length < 2) {
+                div.classList.add("showed");
+                c.showed = true;
+            }
+            
+            if (showed.length === 2) {
+                return;
+            }
+
+            if (showed.length) {
+                const prev = showed[0];
+                const current = c;
+
+                if (prev.i === current.i) {
+                    setTimeout(() => {
+                        prev.div.classList.add('found');
+                        current.div.classList.add('found');
+
+                        prev.div.classList.remove('showed');
+                        current.div.classList.remove('showed');
+
+                        prev.showed = false;
+                        current.showed = false;
+
+                        if (isFirstPlayer) {
+                            player1++;
+                        } else {
+                            player2++;
+                        }
+                    }, 1500);
+                } else {
+                    setTimeout(() => {
+                        prev.div.classList.remove('showed');
+                        current.div.classList.remove('showed');
+
+                        prev.showed = false;
+                        current.showed = false;
+                        
+                        isFirstPlayer = !isFirstPlayer;
+                    }, 1500);
+                }
+            }
         });
     });
 })();
-
